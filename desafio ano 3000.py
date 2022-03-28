d300ef859ed327f85dc9a01a2a159bb07ba5ebc3
ernestopi <90855907+ernestopi@users.noreply.github.com>
2022-03-28 11:07:15 -0300

    Add files via upload

---
+++ desafio ano 3000.py
@@ -0,0 +1,5 @@
+ano_de_nascimento = int(input('Em que ano voçê nasceu? '))
+ano_qualquer = int(input('Qual ano deseja saber sua idade? '))
+idade = (ano_qualquer - ano_de_nascimento)
+print(f'A sua idade no ano de %d será %d.' %(ano_qualquer,idade))
+
---
+++ desafio sua idade em anos de cachorro.py
@@ -0,0 +1,8 @@
+idade = int(input('Quantos anos você tem? '))
+idade_cao = idade*7
+print(f'''Se você fosse um cachorro, você teria {idade_cao} anos!
+ '0'_____'
+    || ||
+  ''')
+
+            
